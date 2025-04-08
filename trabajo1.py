0e95b13fe155a62ed9cab9e1e6058baae2d59557
Joan Cruz <135847775+JoanC9@users.noreply.github.com>
2025-04-07 19:14:30 -0500

    Update trabajo1.py

--- trabajo1.py
+++ trabajo1.py
@@ -2,7 +2,7 @@ meme_dict = {
             "CRINGE": "Algo excepcionalmente raro o embarazoso",
             "LOL": "Una respuesta común a algo gracioso",
             }
-
+#comentario
 word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
 
 if word in meme_dict.keys():
